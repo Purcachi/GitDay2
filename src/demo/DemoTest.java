0b2c0b3e96bce873988406945a637443e72531cd
purca <purca@DESKTOP-BA5KC16>
2022-08-02 14:43:00 -0400

    US123

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -5,6 +5,14 @@ public class DemoTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Test...");
+		
+		}
+		//for US 123...
+		public static void add(int a, int b) {
+			
+			System.out.println(a+b);
+		
+		
 	}
 
 }
